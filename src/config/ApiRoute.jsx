87229434ce2a91927bd062f65ecb77d2b72d9138
Adesh-Dhope <adeshdhope2021@gmail.com>
2026-01-01 10:05:42 +0530

    api file changes

--- src/config/ApiRoute.jsx
+++ src/config/ApiRoute.jsx
@@ -1,4 +1,4 @@
 // export const BackendURL = "https://crud-app-backend-26nj.onrender.com/api";
-// export const BackendURL = "https://first-spring-boot-project-voua.onrender.com/api";
-export const BackendURL = "http://localhost:8080/api";
+export const BackendURL = "https://first-spring-boot-project-voua.onrender.com/api";
+// export const BackendURL = "http://localhost:8080/api";
 
